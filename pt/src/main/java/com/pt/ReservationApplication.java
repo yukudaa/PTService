82dad0ea8545031013f7584eefc4595ee93b0bf3
yukudaa <leeyookoo@naver.com>
2024-02-22 20:41:40 +0900

    feat: 프로젝트 초기 설정

--- pt/src/main/java/com/pt/ReservationApplication.java
+++ pt/src/main/java/com/pt/ReservationApplication.java
@@ -2,8 +2,9 @@ package com.pt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ReservationApplication {
 
 	public static void main(String[] args) {
